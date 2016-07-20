50ce57577a5088ae49bdd3e1692b205518f92321
Tim Holy <tim.holy@gmail.com>
2016-07-20 06:53:16 -0500

    Also test linear indexing

--- test/runtests.jl
+++ test/runtests.jl
@@ -8,7 +8,8 @@ A = IndirectArray(index, colors)
 @test eltype(A) == RGB{U8}
 @test size(A) == (2,2)
 @test ndims(A) == 2
-@test A[1,1] === RGB(1,0,0)
-@test A[1,2] === RGB(0,1,0)
-@test A[2,1] === RGB(0,0,1)
-@test A[2,2] === RGB(1,0,0)
+@test A[1,1] === A[1] === RGB(1,0,0)
+@test A[2,1] === A[2] === RGB(0,0,1)
+@test A[1,2] === A[3] === RGB(0,1,0)
+@test A[2,2] === A[4] === RGB(1,0,0)
+@test isa(eachindex(A), AbstractUnitRange)
