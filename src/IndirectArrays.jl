a07e15515806831ce270e2974597eb993032c163
Andreas Noack <andreasnoackjensen@gmail.com>
2018-01-22 13:14:59 +0100

    Remove disambiguation method since the ambiguity is spurious

--- src/IndirectArrays.jl
+++ src/IndirectArrays.jl
@@ -39,15 +39,6 @@ Base.IndexStyle(::Type{IndirectArray{T,N,A,V}}) where {T,N,A,V} = IndexStyle(A)
 
 Base.copy(A::IndirectArray) = IndirectArray(copy(A.index), copy(A.values))
 
-# To disambiguate
-@inline function Base.getindex(A::IndirectArray{<:Any,1}, i::Int)
-    @boundscheck checkbounds(A.index, i)
-    @inbounds idx = A.index[i]
-    @boundscheck checkbounds(A.values, idx)
-    @inbounds ret = A.values[idx]
-    ret
-end
-
 @inline function Base.getindex(A::IndirectArray, i::Int)
     @boundscheck checkbounds(A.index, i)
     @inbounds idx = A.index[i]
