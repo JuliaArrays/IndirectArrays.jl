b915ef08ac141a6e8b3bff9b60173406bed791f8
Andreas Noack <andreasnoackjensen@gmail.com>
2018-03-11 22:54:22 +0100

    setindex, push, append! and convenience constructor (#12)

--- src/IndirectArrays.jl
+++ src/IndirectArrays.jl
@@ -2,6 +2,8 @@ __precompile__(true)
 
 module IndirectArrays
 
+using Compat
+
 export IndirectArray
 
 """
@@ -26,9 +28,17 @@ end
 Base.@propagate_inbounds IndirectArray(index::AbstractArray{<:Integer,N}, values::AbstractVector{T}) where {T,N} =
     IndirectArray{T,N,typeof(index),typeof(values)}(index, values)
 
+function (::Type{IndirectArray{T}})(A::AbstractArray, values::AbstractVector = unique(A)) where {T}
+    index = convert(Array{T}, indexin(A, values))
+    return IndirectArray(index, values)
+end
+IndirectArray(A::AbstractArray, values::AbstractVector = unique(A)) = IndirectArray{UInt8}(A, values)
+
 Base.size(A::IndirectArray) = size(A.index)
 Base.indices(A::IndirectArray) = indices(A.index)
 Base.IndexStyle(::Type{IndirectArray{T,N,A,V}}) where {T,N,A,V} = IndexStyle(A)
+
+Base.copy(A::IndirectArray) = IndirectArray(copy(A.index), copy(A.values))
 
 @inline function Base.getindex(A::IndirectArray, i::Int)
     @boundscheck checkbounds(A.index, i)
@@ -44,6 +54,40 @@ end
     @boundscheck checkbounds(A.values, idx)
     @inbounds ret = A.values[idx]
     ret
+end
+
+@inline function Base.setindex!(A::IndirectArray, x, i::Int)
+    @boundscheck checkbounds(A.index, i)
+    idx = Compat.findfirst(A.values, x)
+    if idx == nothing
+        push!(A.values, x)
+        A.index[i] = length(A.values)
+    else
+        A.index[i] = idx
+    end
+    return A
+end
+
+@inline function Base.push!(A::IndirectArray{T,1} where T, x)
+    idx = Compat.findfirst(A.values, x)
+    if idx == nothing
+        push!(A.values, x)
+        push!(A.index, length(A.values))
+    else
+        push!(A.index, idx)
+    end
+    return A
+end
+
+function Base.append!(A::IndirectArray{T,1}, B::IndirectArray{T,1}) where T
+    if A.values == B.values
+        append!(A.index, B.index)
+    else # pretty inefficient but let's get something going
+        for b in B
+            push!(A, b)
+        end
+    end
+    return A
 end
 
 end # module
