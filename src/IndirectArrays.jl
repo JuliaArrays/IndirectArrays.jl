6771170865e5a6a60672930fce7f524f740b5c32
Andreas Noack <andreasnoackjensen@gmail.com>
2018-03-15 10:42:02 +0100

    Temporarily add method to work around subtyping bug in Julia 0.6.2

--- src/IndirectArrays.jl
+++ src/IndirectArrays.jl
@@ -40,6 +40,18 @@ Base.IndexStyle(::Type{IndirectArray{T,N,A,V}}) where {T,N,A,V} = IndexStyle(A)
 
 Base.copy(A::IndirectArray) = IndirectArray(copy(A.index), copy(A.values))
 
+if VERSION < v"0.6.3"
+    # This method is only necessary because of a bug in Julia 0.6.2 and can be removed
+    # when we no longer support that version
+    @inline function Base.getindex(A::IndirectArray{<:Any,1}, i::Int)
+        @boundscheck checkbounds(A.index, i)
+        @inbounds idx = A.index[i]
+        @boundscheck checkbounds(A.values, idx)
+        @inbounds ret = A.values[idx]
+        ret
+    end
+end
+
 @inline function Base.getindex(A::IndirectArray, i::Int)
     @boundscheck checkbounds(A.index, i)
     @inbounds idx = A.index[i]
